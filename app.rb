28ccd9c90212c98d28dbe8f0f1ee335e9dc7e16e
Sharon Smith <Sharon Smith>
2016-10-17 22:25:25 -0400

    didnt add images but the rest works. yay!

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
-require_relative 'add_func.rb'
 require 'sinatra'
+require_relative 'coinbank.rb'
 
 get '/' do 
 	erb :get_name
@@ -18,7 +18,7 @@ end
 post '/age' do
 	age = params[:user_age]
 	name = params[:user_name]
-	redirect '/numbers?user_name=' + name + "&user_age=" + age
+	redirect '/numbers?user_name=' + name + '&user_age=' + age
 end
 
 get '/numbers' do
@@ -31,9 +31,42 @@ post '/numbers' do
 	age = params[:user_age]
 	name = params[:user_name]
 	number1 = params[:num1]
-	number2 = params[:num2]
+	number2 = params[:num2]	
 	number3 = params[:num3]
-	sum = add(number1.to_i, number2.to_i, number3.to_i)
 
-"Here's what I know so far.  Your name is #{name}, you are #{age} years old, and your favorite numbers are #{number1}, #{number2}, and #{number3}, which totals #{sum} when added together. Do you want to tell me more?"
+	redirect '/amount?user_name=' + name + '&user_age=' + age + '&num1=' + number1 + '&num2=' + number2 + '&num3=' + number3 
+end
+
+#something breaks this at line 37. TypeError at numbers/ no implicit conversion of nil into string
+
+get '/amount' do
+	age = params[:user_age]
+	name = params[:user_name]
+	number1 = params[:num1].to_i
+	number2 = params[:num2].to_i	
+	number3 = params[:num3].to_i
+	total = number1 + number2 + number3
+	
+	erb :get_amount, :locals => {:name => name, :age => age, :number1 => number1, :number2 => number2, :number3 => number3, :total => total}
+end
+
+post '/amount' do
+	name = params[:user_name]
+	user_money = params[:money_tendered].to_i
+	coins = change(user_money)
+	results = coinbank(coins)
+
+	erb :results, :locals => {:name => name, :user_money => user_money, :results => results }
+end
+	
+get '/Contact' do
+	erb :contact
+end
+
+get '/About' do
+	erb :about
+end
+
+get '/History' do
+	erb :history
 end
--- coinbank.rb
+++ coinbank.rb
@@ -1,13 +1,34 @@
-def coins_in_change(cents_in_change) 
- 	coins = {}
+def change(cents_received) 
+ 	#coins = {}
 
- 	money = {:ten => 1000, :five => 500, :one => 100, :half_dollar => 50, :quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
- 	money.each do |coin_type, coin_value|
+ 	# money = {:ten => 1000, :five => 500, :one => 100, :half_dollar => 50, :quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
+ 	# money.each do |coin_type, coin_value|
 
- 		if cents_in_change >= coin_value
-  			coins[coin_type] = cents_in_change / coin_value
- 			cents_in_change = cents_in_changec % coin_value
+ 	# 	if cents_in_change >= coin_value
+  # 			coins[coin_type] = cents_in_change / coin_value
+ 	# 		cents_in_change = cents_in_changec % coin_value
+
+ 	coins = {penny: 0, nickle: 0, dime: 0, quarter: 0, half_dollar: 0, one: 0, five: 0, ten: 0, twenty: 0}
+	coin_value = {twenty: 2000, ten: 1000, five: 500, one: 100, half_dollar: 50, quarter: 25, dime: 10, nickle: 5, penny: 1}
+	
+
+	coin_value.each do | coin, value|
+      while
+      	value <= cents_received
+      	coins[coin] += 1
+      	cents_received = cents_received - value
  		end
- 		coins
  	end
+ 	coins
  end
+
+ def coinbank(coins)
+	change = ""
+	coins.each do |key, value|
+		if value > 0
+		change << " " + value.to_s + " " + key.to_s+ "'s"
+	    end
+		
+	end
+	change
+end
